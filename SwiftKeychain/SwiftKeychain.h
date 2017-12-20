5517d7d35d891e3c4173c56e23ccc33ec6823aee
Steve Brokaw <brokaw@twocanoes.com>
2017-12-20 09:23:46 -0600

    Initial Commit

---
+++ SwiftKeychain/SwiftKeychain.h
@@ -0,0 +1,19 @@
+//
+//  SwiftKeychain.h
+//  SwiftKeychain
+//
+//  Created by Steve Brokaw on 12/20/17.
+//  Copyright © 2017 Steve Brokaw. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftKeychain.
+FOUNDATION_EXPORT double SwiftKeychainVersionNumber;
+
+//! Project version string for SwiftKeychain.
+FOUNDATION_EXPORT const unsigned char SwiftKeychainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftKeychain/PublicHeader.h>
+
+
